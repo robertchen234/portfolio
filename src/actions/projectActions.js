7037246222fcdecbe5e3da1c3e42d05791867a17
rob <robertchen234@gmail.com>
2019-03-27 20:29:39 -0400

    fetch heroku url

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,7 +1,7 @@
 export const LOAD_PROJECTS = "LOAD_PROJECTS";
 export const getProjects = () => {
   return dispatch => {
-    return fetch("http://localhost:3000/api/v1/projects")
+    return fetch("https://robert-chen-backend.herokuapp.com/api/v1/projects")
       .then(res => res.json())
       .then(projects => {
         dispatch({

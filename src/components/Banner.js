3e44e7103cd344a11251c0b697119ae62c61eea5
rob <robertchen234@gmail.com>
2019-03-31 21:48:48 -0400

    banner click scrolls down

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,13 +2,12 @@ import React from "react";
 
 const Banner = props => {
   return (
-    <div className="header-break-footer" style={ { backgroundImage: `url(/assets/${props.bannerImage})` } }>
+    <div className="header-break-footer" onClick={() => props.scrollTo()} style={ { backgroundImage: `url(/assets/${props.bannerImage})` } }>
       <div className="banner-text scroll-text">
         <h1>Robert</h1>
         <h1>Chen</h1>
         <span
           className="pointer center scroll"
-          onClick={() => props.scrollTo()}
         >
           <img src="/assets/down-arrow.png" alt="down arrow" />
         </span>

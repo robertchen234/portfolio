2ba15e3d98a8292b882cd7a604dd96d13c788550
Robert Chen <robert.rob.chen@gmail.com>
2020-08-04 22:11:51 -0400

    Revert "Revert "feat(projects): changed demo url to netlify app""
    This reverts commit 21bcf081692ebf304c315423f3a926421959a94e.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,118 +1,67 @@
-import React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { addClap } from "../actions/projectActions";
-import { Link } from "react-scroll";
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addClap } from '../actions/projectActions';
+import { Link } from 'react-scroll';
 
-const ProjectCard = props => {
-  const project = props.project;
-  return (
-    <div
-      className={`${project.title
-        .toLowerCase()
-        .split(" ")
-        .join("-")} project-card-background`}
-    >
-      <div className="dark-overlay">
-        <div id="projects" className="project-card">
-          <Link
-            activeClass="active"
-            to="projects"
-            spy={true}
-            smooth={true}
-            offset={-253}
-            duration={500}
-          >
-            <div
-              className="go-back pointer"
-              onClick={() => props.toggleClick(project)}
-            >
-              <img src="/assets/backarrow.png" alt="back arrow" />
-            </div>
-          </Link>
-          <div className="right details">
-            <h2 className="center">{project.title}</h2>
-            <h5 className="center teaser">{project.teaser}</h5>
-            <h6 className="center">{project.year}</h6>
-            <h1 />
-            <p>{project.description}</p>
-            <p>Technology Stack: {project.tech_stack}</p>
-            <span className="buttons">
-              {project.demo_url ? (
-                <a
-                  href={`${project.demo_url}`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <h4 className="pointer button dark-surround black">
-                    Website
-                  </h4>
-                </a>
-              ) : null}
-              {project.git_url ? (
-                <a
-                  href={`${project.git_url}`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <h4 className="pointer button dark-surround black">GitHub</h4>
-                </a>
-              ) : null}
-            </span>
-            <div
-              onClick={() => props.addClap(project)}
-              className="pointer center claps"
-            >
-              <span role="img" aria-label="clap">
-                👏
-              </span>
-              <span className="black">
-                {props.projects.find(proj => proj.id === project.id).claps}
-              </span>
-            </div>
-          </div>
-          <div className="left asset">
-            {project.video_url ? (
-              <iframe
-                title={project.title}
-                className="youtube"
-                width="100%"
-                height="400"
-                src={project.video_url}
-                frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-            ) : (
-              <a
-                href={`${project.demo_url}`}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <img
-                  width="100%"
-                  className="image pointer"
-                  src={`/assets/${project.image_url}`}
-                  alt={project.title}
-                />
-              </a>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+const ProjectCard = (props) => {
+	const project = props.project;
+	return (
+		<div className={`${project.title.toLowerCase().split(' ').join('-')} project-card-background`}>
+			<div className="dark-overlay">
+				<div id="projects" className="project-card">
+					<Link activeClass="active" to="projects" spy={true} smooth={true} offset={-253} duration={500}>
+						<div className="go-back pointer" onClick={() => props.toggleClick(project)}>
+							<img class="back-arrow" src="/assets/back.png" alt="back arrow" />
+						</div>
+					</Link>
+					<div className="right details">
+						<h2 className="center">{project.title}</h2>
+						<h5 className="center teaser">{project.teaser}</h5>
+						<h6 className="center">{project.year}</h6>
+						<h1 />
+						<p>{project.description}</p>
+						<p>Technology Stack: {project.tech_stack}</p>
+						<span className="buttons">
+							{project.demo_url ? (
+								<a href={`${project.demo_url}`} rel="noopener noreferrer" target="_blank">
+									<h4 className="pointer button dark-surround black">Website</h4>
+								</a>
+							) : null}
+							{project.git_url ? (
+								<a href={`${project.git_url}`} rel="noopener noreferrer" target="_blank">
+									<h4 className="pointer button dark-surround black">GitHub</h4>
+								</a>
+							) : null}
+						</span>
+						<div onClick={() => props.addClap(project)} className="pointer center claps">
+							<span role="img" aria-label="clap">
+								👏
+							</span>
+							<span className="black">{props.projects.find((proj) => proj.id === project.id).claps}</span>
+						</div>
+					</div>
+					<div className="left asset">
+						{project.video_url ? (
+							<iframe title={project.title} className="youtube" width="100%" height="400" src={project.video_url} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+						) : (
+							<a href={`${project.demo_url}`} rel="noopener noreferrer" target="_blank">
+								<img width="100%" className="image pointer" src={`/assets/${project.image_url}`} alt={project.title} />
+							</a>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-const mapStateToProps = state => {
-  return { projects: state.projects };
+const mapStateToProps = (state) => {
+	return { projects: state.projects };
 };
 
-const mapDispatchToProps = dispatch => {
-  return { addClap: bindActionCreators(addClap, dispatch) };
+const mapDispatchToProps = (dispatch) => {
+	return { addClap: bindActionCreators(addClap, dispatch) };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ProjectCard);
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectCard);

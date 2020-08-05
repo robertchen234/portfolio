21bcf081692ebf304c315423f3a926421959a94e
Robert Chen <robert.rob.chen@gmail.com>
2020-08-04 22:00:13 -0400

    Revert "feat(projects): changed demo url to netlify app"
    This reverts commit 1725669653ff593c40a92c4bfa1b745b85453e8b.

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { Link } from 'react-scroll';
+import React from "react";
+import { Link } from "react-scroll";
 
-const ProjectImage = (props) => {
-	const project = props.project;
-	return (
-		<div className="project-div pointer" className="f1_container">
-			<Link activeClass="active" to="projects" spy={true} smooth={true} offset={253} duration={500} onClick={() => props.toggleClick(project)}>
-				<div className="flipper shadow" className="f1_card">
-					<div className="front face">
-						<img className="project-image" src={`/assets/${project.image_url}`} alt={project.title} />
-					</div>
-					<div className="back face center">
-						<h4>{project.year}</h4>
-						<p>{project.tech_stack}</p>
-					</div>
-				</div>
-				<div className="project-title">
-					<h3>{project.title}</h3>
-					<p>{project.teaser}</p>
-				</div>
-			</Link>
-		</div>
-	);
+const ProjectImage = props => {
+  const project = props.project;
+  return (
+    <div className="project-div pointer" id="f1_container">
+      <Link
+        activeClass="active"
+        to="projects"
+        spy={true}
+        smooth={true}
+        offset={253}
+        duration={500}
+        onClick={() => props.toggleClick(project)}
+      >
+        <div className="flipper shadow" id="f1_card">
+          <div className="front face">
+            <img
+              className="project-image"
+              src={`/assets/${project.image_url}`}
+              alt={project.title}
+            />
+          </div>
+          <div className="back face center">
+            <h4>{project.year}</h4>
+            <p>{project.tech_stack}</p>
+          </div>
+        </div>
+        <div className="project-title">
+          <h3>{project.title}</h3>
+          <p>{project.teaser}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectImage;
